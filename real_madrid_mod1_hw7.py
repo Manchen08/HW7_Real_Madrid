489573ec1aa8e7a33cc5439d48a64e31b404f111
Theodore Laser <theodorelaser@mail.weber.edu>
2017-03-28 13:05:27 -0600

    Added psudo code for mod1 and mod2.

--- real_madrid_mod1_hw7.py
+++ real_madrid_mod1_hw7.py
@@ -1,12 +1,34 @@
 #!/usr/bin/env python3
 import sys
 
+
+def unlock():
+    """
+    Test the condition given to see if the side doors of the van can be opened.
+    """
+    #read in all the values
+    #LD = Left Dashboard switch
+    #RD = Right Dashboard switch
+    #CL = Child Lock switch
+    #ML = Master Unlock switch
+    #LI = Left Inside handle
+    #LO = Left Outside handle
+    #RI = Right Inside handle
+    #RO = Right Inside handle
+    #GS = Gear Shift Position (Valid values: P, N, D, 1, 2, 3, R
+    #validate GS with try/except, if not in valid values print that error
+    #check if GS = P, if not doors cannot open
+    #check if ML is on or off, if ML is on doors cannot open
+    #if GS and ML pass, check each left and right door options
+
+
 #Main function
 def main():
     """
     Test Function
     """
     pass
+
 
 if __name__ == "__main__":
     # Call Main
--- real_madrid_mod2_hw7.py
+++ real_madrid_mod2_hw7.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
 import sys
 
+
+def i_can_open():
+    """
+    Pass the values given to check if doors can unlock:
+    """
+    #import module2 file
+    #prompt for file
+    #loop line by line and pass each line to Module1.py file
+    #   print out record being read
+    #   print out each switch state
+    #   print whether the doors can open
+
+
 #Main function
 def main():
     """
