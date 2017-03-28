5bc6fb3de8fe4918e8fa5fd75e9d9a41f16a6d40
Jon Wheeler <jonathanwheeler1@weber.edu>
2017-03-28 13:21:07 -0600

    Pushing changes

--- real_madrid_mod3_hw7.py
+++ real_madrid_mod3_hw7.py
@@ -1,15 +1,30 @@
+"""
+A module to print out a given zip code as a bar code 
+"""
+
 #!/usr/bin/env python3
 import sys
+import re
+
+def printDigit(d):
+    pass
+
+def printBarCode(zipCode):
+    pass
 
 #Main function
 def main():
     """
     Test Function
     """
-    pass
+    i= input("Please enter the zip code")
+    while not i.isdigit() or len(i) != 5:
+        print("Only digits are allowed and must be 5 characters")
+        i = input("Please enter the zip code")
+
+
 
 if __name__ == "__main__":
     # Call Main
-    main()
-
+    main() 
     exit(0)
