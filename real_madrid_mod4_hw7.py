0332da9ed5e0f32f68ac3cd4b3f173ead5d773ba
Jon Wheeler <jonathanwheeler1@weber.edu>
2017-03-28 12:49:31 -0600

    Adding mod

---
+++ real_madrid_mod4_hw7.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import sys
+
+#Main function
+def main():
+    """
+    Test Function
+    """
+    pass
+
+if __name__ == "__main__":
+    # Call Main
+    main()
+
+    exit(0)
